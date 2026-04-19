cf03886aa717156936c26e8076557c9417db57ee
Krishna C <krishnacgowda10@gmail.com>
2026-04-19 20:50:12 +0530

    chore: remove react-query-devtools from providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
 import { useState } from 'react'
 import { TrialModalProvider } from '@/app/_components/TrialModalContext'
 
@@ -20,7 +20,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
       <TrialModalProvider>
         {children}
       </TrialModalProvider>
-      {process.env.NODE_ENV === 'development' && <ReactQueryDevtools />}
+
     </QueryClientProvider>
   )
 }
